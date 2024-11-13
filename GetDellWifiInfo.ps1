ab9aa78c89f4f0af3918db51904b0e4abc04aa6f
Scott <Wheelerscott@hotmail.com>
2024-11-13 10:43:35 -0700

    Update GetDellWifiInfo.ps1
    Combined both previous scripts

--- GetDellWifiInfo.ps1
+++ GetDellWifiInfo.ps1
@@ -1,4 +1,4 @@
-# Expanded Network Information Script
+# Comprehensive System and Network Information Script
 # Run as Administrator
 
 # System Information
@@ -6,43 +6,40 @@ $SystemInfo = Get-CimInstance -ClassName Win32_OperatingSystem
 $OSName = $SystemInfo.Caption
 $OSVersion = $SystemInfo.Version
 $OSBuild = $SystemInfo.BuildNumber
+$ServicePack = $SystemInfo.ServicePackMajorVersion
+$OSInstallDate = $SystemInfo.InstallDate
+$LastBootUpTime = (Get-CimInstance -ClassName Win32_OperatingSystem).LastBootUpTime
+
+# Windows Updates
+$Updates = Get-HotFix | Select-Object -Property Description, HotFixID, InstalledOn
 
 # IP Configuration and Network Adapter Details
 $IPConfig = Get-NetIPAddress | Where-Object { $_.AddressFamily -eq "IPv4" }
 $DNSInfo = Get-DnsClientServerAddress | Select-Object -Property InterfaceAlias, ServerAddresses
 
-# Wireless Adapter Signal Quality and Network Name
+# Wireless Adapter Details
 $WiFiAdapters = Get-NetAdapter -Physical | Where-Object { $_.Name -like "*Wi-Fi*" -or $_.Name -like "*Wireless*" }
-$WiFiDetails = @()
+$WirelessInfo = @()
 foreach ($adapter in $WiFiAdapters) {
     $NetStats = Get-NetAdapterStatistics -Name $adapter.Name
     $WiFiNetwork = Get-NetConnectionProfile | Where-Object { $_.InterfaceAlias -eq $adapter.Name }
-    $WiFiDetails += [PSCustomObject]@{
+    $DriverInfo = Get-WmiObject Win32_PnPSignedDriver | Where-Object { $_.DeviceID -eq $adapter.PnPDeviceID }
+    $WirelessInfo += [PSCustomObject]@{
         AdapterName = $adapter.Name
         MACAddress = $adapter.MacAddress
         Status = $adapter.Status
         LinkSpeed = $adapter.LinkSpeed
         SignalQuality = $NetStats.ReceiveQuality
         NetworkName = $WiFiNetwork.Name
+        DriverProvider = $DriverInfo.ProviderName
+        DriverVersion = $DriverInfo.DriverVersion
+        DriverDate = $DriverInfo.DriverDate
     }
 }
 
 # Active TCP Connections
 $TCPConnections = Get-NetTCPConnection | Where-Object { $_.State -eq "Established" } | 
     Select-Object -Property LocalAddress, LocalPort, RemoteAddress, RemotePort, State
-
-# Network Adapter Driver Information
-$DriverInfo = Get-WmiObject Win32_PnPSignedDriver | Where-Object { $_.DeviceClass -eq "Net" }
-$AdapterDriverInfo = @()
-foreach ($adapter in $WiFiAdapters) {
-    $Driver = $DriverInfo | Where-Object { $_.DeviceID -eq $adapter.PnPDeviceID }
-    $AdapterDriverInfo += [PSCustomObject]@{
-        AdapterName = $adapter.Name
-        DriverProvider = $Driver.ProviderName
-        DriverVersion = $Driver.DriverVersion
-        DriverDate = $Driver.DriverDate
-    }
-}
 
 # Recent Network Events (Last 24 hours)
 $NetworkEvents = Get-WinEvent -LogName System | Where-Object { $_.TimeCreated -gt (Get-Date).AddHours(-24) -and $_.Id -in 4000..4999 } |
@@ -53,6 +50,13 @@ Write-Output "==== System Information ===="
 Write-Output "OS Name: $OSName"
 Write-Output "OS Version: $OSVersion"
 Write-Output "OS Build: $OSBuild"
+Write-Output "Service Pack: $ServicePack"
+Write-Output "OS Install Date: $OSInstallDate"
+Write-Output "Last Boot Up Time: $LastBootUpTime"
+
+Write-Output "`n==== Installed Windows Updates ===="
+$Updates | Format-Table -AutoSize
+
 Write-Output "`n==== IP Configuration ===="
 $IPConfig | Format-Table -AutoSize
 
@@ -60,13 +64,10 @@ Write-Output "`n==== DNS Information ===="
 $DNSInfo | Format-Table -AutoSize
 
 Write-Output "`n==== Wireless Adapter Details ===="
-$WiFiDetails | Format-Table -AutoSize
+$WirelessInfo | Format-Table -AutoSize
 
 Write-Output "`n==== Active TCP Connections ===="
 $TCPConnections | Format-Table -AutoSize
-
-Write-Output "`n==== Network Adapter Driver Information ===="
-$AdapterDriverInfo | Format-Table -AutoSize
 
 Write-Output "`n==== Recent Network Events (Last 24 hours) ===="
 $NetworkEvents | Format-Table -AutoSize
